3b06cfe587523b8ef54473dfb315556e956ffc2c
Wonder0378 <marcusinskolan@gmail.com>
2021-11-17 13:36:07 +0100

    Semi complete

--- Inlämningsuppgifter/Miniraknare/miniraknare.js
+++ Inlämningsuppgifter/Miniraknare/miniraknare.js
@@ -1,1 +1,17 @@
 
+// Sets display constant
+var display = document.getElementById("screen");
+
+//Main function for input (numbers/operators)
+function button(strVar) {
+    if(strVar == "clear") {
+        display.innerHTML = "";
+    } else {
+        display.innerHTML = 
+        display.innerHTML + strVar;
+    }
+};
+//Calculate function
+function calc() {
+    display.innerHTML = eval(display.innerHTML);
+};
