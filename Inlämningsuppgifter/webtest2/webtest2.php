e4cd7721c7f2255fc204bb26395ddeac81335f32
Wonder0378 <marcusinskolan@gmail.com>
2021-12-02 21:28:58 +0100

    webtest2

---
+++ Inlämningsuppgifter/webtest2/webtest2.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+        body {
+            position: absolute;
+            height: 100vh;
+            width: 100%;
+        }
+        header {
+            height: 100px;
+            width: 100%;
+        }
+        .pic {
+            height: 300px;
+            width: 200px;
+        }
+        img {
+            height: 300px;
+            width: fit-content;
+        }
+        .maintext {
+            position: absolute;
+            width: 
+        }
+    </style>
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Filmrecension Rubrik</h1>
+        <p>Av Marcus Hedquist</p>
+    </header>
+    <div class="pic">
+        <img src="https://s.studiobinder.com/wp-content/uploads/2019/06/Movie-Poster-Template-Movie-Credits-StudioBinder.jpg.webp" alt="">
+        <p>Bildtext</p>
+    </div>
+    <div class="maintext">
+        <p id="ingress"><strong>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Enim magnam architecto sit suscipit. Dolore eos, sapiente vel perspiciatis 
+            suscipit itaque delectus autem qui, eius quae, quis mollitia quam assumenda? Illo.</strong></p>
+        <p id="brodtext">Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Deleniti ipsum laborum, placeat qui quidem beatae porro iusto quaerat 
+            maxime a! Saepe temporibus recusandae impedit animi quam, iste alias expedita 
+            provident.At quaerat vel earum harum enim asperiores nulla quibusdam excepturi esse doloribus. 
+            Assumenda totam error quia iure esse corrupti maiores aut cumque laboriosam, impedit 
+            tempore voluptas perspiciatis officia voluptatibus explicabo?</p>
+    </div>
+    <footer>
+        <p>Eftermiddagsbladet</p>
+    </footer>
+</body>
+</html>
